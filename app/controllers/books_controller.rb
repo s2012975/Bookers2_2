1066a74fa6f6540683118f2839ba5ce7b814f28f
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2021-03-07 03:43:05 +0000

    [add]モデルとコントローラを追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def create
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'homes/about'
+  devise_for :users
+  resources :users, except: [:new, :create, :destroy]
+  resources :books, except: :new
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
